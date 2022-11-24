20056e496de5fbb7179c66b144b2c0d1a717e11d
WESLEY SHI <shiweijian0818@gmail.com>
2022-11-24 16:13:08 -0500

    database

--- schema.sql
+++ schema.sql
@@ -3,24 +3,23 @@ CREATE TABLE IF NOT EXISTS TAXI (
    taxiId INTEGER PRIMARY KEY AUTOINCREMENT,
    tpep_pickup_datetime DATE,
    tpep_dropoff_datetime DATE,
-   trip_distance REAL,
-   pick_lon REAL,
-   pick_lat REAL,
-   drop_lon REAL,
-   drop_lat REAL,
-   distance REAL,
-   tip_amount REAL
+   trip_distance FLOAT,
+   pick_lon FLOAT,
+   pick_lat FLOAT,
+   drop_lon FLOAT,
+   drop_lat FLOAT,
+   distance FLOAT,
+   tip_amount FLOAT
 );
 
 CREATE TABLE IF NOT EXISTS TAXI (
    uberId INTEGER PRIMARY KEY AUTOINCREMENT,
-   fare_amount REAL,
+   fare_amount FLOAT,
    pickup_datetime DATE,
-   distance REAL,
-   pick_lon REAL,
-   pick_lat REAL,
-   drop_lon REAL,
-   drop_lat REAL,
-   distance REAL,
+   distance FLOAT,
+   pick_lon FLOAT,
+   pick_lat FLOAT,
+   drop_lon FLOAT,
+   drop_lat FLOAT,
    passenger_count INTEGER
 );
