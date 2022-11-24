48f73c86c785c68b923128875c5025fa645a65e7
jiashaoxiong <sj3216@columbia.edu>
2022-11-25 02:46:27 +0800

    UPDATE

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 
+TAXI_TRIPS_SCHEMA = """
 CREATE TABLE IF NOT EXISTS TAXI (
    taxiId INTEGER PRIMARY KEY AUTOINCREMENT,
    tpep_pickup_datetime DATE,
@@ -11,8 +12,10 @@ CREATE TABLE IF NOT EXISTS TAXI (
    distance REAL,
    tip_amount REAL
 );
+"""
 
-CREATE TABLE IF NOT EXISTS TAXI (
+UBER_TRIPS_SCHEMA = """
+CREATE TABLE IF NOT EXISTS UBER (
    uberId INTEGER PRIMARY KEY AUTOINCREMENT,
    fare_amount REAL,
    pickup_datetime DATE,
@@ -24,3 +27,36 @@ CREATE TABLE IF NOT EXISTS TAXI (
    distance REAL,
    passenger_count INTEGER
 );
+"""
+
+HOURLY_WEATHER_SCHEMA="""
+CREATE TABLE IF NOT EXISTS HOURLY (
+   REM TEXT PRIMARY KEY, 
+   DATE TEXT,
+   REPORT_TYPE	TEXT,
+   HourlyAltimeterSetting REAL,
+   HourlyDewPointTemperature REAL,
+   HourlyDryBulbTemperature REAL,
+   HourlyRelativeHumidity	REAL,
+   HourlySeaLevelPressure REAL,
+   HourlyVisibility REAL,
+   HourlyWindDirection REAL,
+   HourlyWindSpeed REAL
+
+);
+"""
+
+DAILY_WEATHER_SCHEMA="""
+CREATE TABLE IF NOT EXISTS DAILY(
+   
+   DATE TEXT PRIMARY KEY,
+   DailyAverageDewPointTemperature REAL,
+   DailyAverageDryBulbTemperature REAL,
+   DailyMaximumDryBulbTemperature REAL,
+   DailyMinimumDryBulbTemperature REAL,
+   DailyAverageRelativeHumidity REAL,
+   DailyAverageSeaLevelPressure REAL,
+   DailyAverageWindSpeed REAL,
+   DailyPeakWindSpeed REAL
+);
+"""
